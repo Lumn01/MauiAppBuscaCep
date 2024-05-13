9ca1aba2c1ca31ff545313b7cbd6bd73d930ab3b
Usuario <Usuario@CPD03-M16>
2024-05-13 12:22:53 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppBuscaCep/Models/Endereco.cs
@@ -0,0 +1,18 @@
+﻿namespace MauiAppBuscaCep.Models
+{
+    public class Endereco
+    {
+        public int id_logradouro { get; set; }
+        public int id_cidade { get; set; }
+        public string tipo { get; set; }
+        public string descricao { get; set; }
+        public string uf { get; set; }
+        public string complemento { get; set; }
+        public string descricao_com_numero { get; set; }
+        public string descricao_cidade { get; set; }
+        public string descricao_bairro { get; set; }
+        public object rows { get; set; }
+        public int CEP { get; set; }
+        public string UF { get; set; }
+    }
+}
